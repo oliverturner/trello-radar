6d956dd6247a52ec38ca60d052b0b042b69360d5
Oliver Turner <Oliver.Turner@Ticketmaster.co.uk>
2015-12-07 15:55:08 +0000

    UX tweaks to reflect new quadrants

--- app/components/chart/quadrant-label.jsx
+++ app/components/chart/quadrant-label.jsx
@@ -8,7 +8,7 @@ const QuadrantLabel = ({name, arcId}) => {
   // Crashes Firefox! @see https://bugzilla.mozilla.org/show_bug.cgi?id=1067695
   const path = `<textPath xlink:href="#${arcId}">${name}</textPath>`
   const dx   = (Math.PI * metrics.horizonWidth * 2) / (metrics.quadNum * 2)
-  const dy   = -15
+  const dy   = -10
 
   return (
     <text className={styles['quadrantlabel']} dx={dx} dy={dy} dangerouslySetInnerHTML={{__html: path }}/>
