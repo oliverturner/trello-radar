39b35dad3a1e646ef940f4dc31d9663fa9c81cf2
Oliver Turner <Oliver.Turner@Ticketmaster.co.uk>
2015-12-14 19:26:36 +0000

    Minor updates to deps and code

--- app/index.jsx
+++ app/index.jsx
@@ -9,7 +9,6 @@ import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
 import reducer from './reducers'
-//import {srcs, types, searchQS} from './utils/trello'
 import {loadData} from './actions/trello'
 
 import Application from './components/application'
@@ -17,31 +16,15 @@ import Application from './components/application'
 // Start
 //-----------------------------------------------
 // create a store that has redux-thunk middleware enabled
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+const createAsyncStore = applyMiddleware(thunk)(createStore)
 
-const store = createStoreWithMiddleware(reducer, {
+const store = createAsyncStore(reducer, {
   horizonSelected:   null,
   cardSelected:      null,
   cardHovered:       null,
   cardsFiltered:     [],
   textPathSupported: navigator.userAgent.toLowerCase().indexOf('firefox') === -1
 })
-
-const onError = (err) => {
-  console.log(err)
-}
-
-//const loadData = () => (dispatch) =>
-//  Promise
-//    .all(srcs.map((src) => fetch(src).then((res) => res.json())))
-//    .then((values) =>
-//      values.reduce((ret, val, index) => {
-//        ret[types[index]] = val
-//        return ret
-//      }, {})
-//    )
-//    .then((data) => dispatch({type: 'DATA_LOADED', payload: data}))
-//    .then(() => dispatch({type: 'DATA_DERIVED'}))
 
 // Let's go disco!
 //-----------------------------------------------
@@ -54,4 +37,4 @@ store.dispatch(loadData())
       document.getElementById('app')
     )
   })
-  .catch(onError)
+  .catch((err) => console.log(err))
